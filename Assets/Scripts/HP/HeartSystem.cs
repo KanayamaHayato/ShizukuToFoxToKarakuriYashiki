b40ce3cb8ac6b9c64e650dbe09570b81c6aac355
KanayamaHayato <241205055@ccmailg.meijo-u.ac.jp>
2026-06-09 12:37:51 +0900

    怪物の攻撃モーションの追加
    怪物のランダムスポーンも追加した
    捕まった後にカメラズームからのダメージを作成
    しかし、ダメージ判定が二回呼び出されているので要修正

--- Assets/Scripts/HP/HeartSystem.cs
+++ Assets/Scripts/HP/HeartSystem.cs
@@ -34,7 +34,7 @@ public class HeartSystem : MonoBehaviour
         UpdateHearts();
     }
 
-    void UpdateHearts()
+    public void UpdateHearts()
     {
         for (int i = 0; i < hearts.Length; i++)
         {
